db33f268d421a7b72c4ebb77ae33da6fa222ccba
Jordan Hawker <hawker.jordan@gmail.com>
2015-08-15 14:39:11 -0700

    Upgrade to ember-cli 1.13.8

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -10,7 +10,8 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
   This Brocfile does *not* influence how the addon or the app using it
   behave. You most likely want to be modifying `./index.js` or app's Brocfile
 */
+module.exports = function (defaults) {
+  var app = new EmberAddon();
 
-var app = new EmberAddon();
-
-module.exports = app.toTree();
+  return app.toTree();
+};
