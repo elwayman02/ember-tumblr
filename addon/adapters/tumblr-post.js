1ba1861a87b611e7a41e727fa5250e381fff7581
Jordan Hawker <hawker.jordan@gmail.com>
2015-08-16 02:22:46 -0700

    Add documentation for tumblr-post & tumblr-blog

--- addon/adapters/tumblr-post.js
+++ addon/adapters/tumblr-post.js
@@ -2,17 +2,56 @@ import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
+  /**
+   * Tumblr Application API Key
+   * @type {string}
+   * @default
+   */
   apiKey: '',
+
+  /**
+   * Tumblr Blog URL
+   * @type {string}
+   * @default
+   */
   blogUrl: '',
-  type: '',
+
+  /**
+   * Tumblr API Hostname
+   * @type {string}
+   * @default
+   */
   host: 'https://api.tumblr.com',
+
+  /**
+   * Post Type
+   * @type {string}
+   * @default
+   */
+  type: '',
+
+  /**
+   * Build path from type
+   * @returns {string} API path with type string appended
+   */
   pathForType() {
     const type = Ember.isPresent(this.get('type')) ? `/${this.get('type')}` : '';
     return `posts${type}`;
   },
-  namespace: Ember.computed(function () {
+
+  /**
+   * Build namespace from blogURL
+   * @returns {string} Tumblr API namespace with blogURL appended
+   */
+  namespace: Ember.computed('blogURL', function () {
     return `v2/blog/${this.get('blogUrl')}`;
   }),
+
+  /**
+   * Build hash for AJAX call
+   * Appends API key and sets data type
+   * @returns {object} hash of AJAX options
+   */
   ajaxOptions() {
     const hash = this._super.apply(this, arguments);
     hash.data = hash.data || {};
