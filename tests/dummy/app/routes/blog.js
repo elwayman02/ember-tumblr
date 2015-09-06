d454325de0e04043b4178d305c6907df9161e855
Jordan Hawker <hawker.jordan@gmail.com>
2015-09-05 20:52:21 -0700

    Upgrade to Ember-Data 1.13.11

--- tests/dummy/app/routes/blog.js
+++ tests/dummy/app/routes/blog.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.find('tumblr-post-text');
+    return this.store.findAll('tumblr-post-text');
   }
 });
--- tests/dummy/app/routes/post.js
+++ tests/dummy/app/routes/post.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params/*, transition*/) {
-    return this.store.find('tumblr-post-text', {
+    return this.store.findAll('tumblr-post-text', {
       id: params.post_id
     }).then(function (posts) {
       return posts.get('firstObject');
