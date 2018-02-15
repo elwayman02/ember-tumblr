dde7f2230dcfd03680c9a300dc38fcea10826c9e
Chris Nevill <chris@leftoverbits.co.uk>
2018-02-15 12:37:07 +0000

    tumblr-photo-feature

--- tests/unit/models/tumblr-post-test.js
+++ tests/unit/models/tumblr-post-test.js
@@ -1,7 +1,7 @@
 import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('tumblr-post', 'Unit | Model | tumblr-post', {
-  // Specify the other units that are required for this test.
+  // Specify the other units that are required for this test.  
   needs: []
 });
 
